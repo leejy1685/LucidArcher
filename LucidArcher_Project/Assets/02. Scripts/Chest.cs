924e295a69e56b261b0be9132607e55e819a24f7
hbks0224 <hbks1234@gmail.com>
2025-05-08 19:05:04 +0900

    상자 오픈 애니메이션

---
+++ LucidArcher_Project/Assets/02. Scripts/Chest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+   public Animator animator;
+    
+
+    public void DestroyChest()
+    {
+        Destroy(gameObject, 1.0f);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            animator.SetTrigger("Open");
+
+
+        
+
+        }
+            
+
+
+
+        
+
+
+    }
+}
