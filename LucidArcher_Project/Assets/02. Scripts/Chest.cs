74c072189df0a42df102e80e6cf6f72add87c3a0
hbks0224 <hbks1234@gmail.com>
2025-05-08 20:32:17 +0900

    아이템 수정

--- LucidArcher_Project/Assets/02. Scripts/Chest.cs
+++ LucidArcher_Project/Assets/02. Scripts/Chest.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
    public Animator animator;
-    public GameObject[] itemPrefabs;
+    public GameObject[] itemPrefabs; //아이템 목록 담을 리스트
 
-    public int itemcount = 5 ;
-    public float dropForce = 2f;
+     
+    public float dropForce = 2f; //아이템 튀어오를때 힘
     
     
 
-    public void DestroyChest()
+    public void DestroyChest() // 상자 제거
     {
         Destroy(gameObject, 1.0f);
 
@@ -22,7 +22,7 @@ public class Chest : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
 
-            animator.SetTrigger("Open");
+            animator.SetTrigger("Open"); //트리거 발생 시 상자여는 애니메이션 재생
 
 
             //DropItems();
@@ -36,16 +36,18 @@ public class Chest : MonoBehaviour
     void DropItems()
     {
 
-        for (int i = 0; i < itemcount; i++)
+             int itemcount = Random.Range(1,6) ; //드랍 아이템 개수
+
+        for (int i = 0; i < itemcount; i++) // 드랍 아이템 수 만큼 반복
         {
-            GameObject item_prefab = itemPrefabs[Random.Range(0, itemPrefabs.Length)];
+            GameObject item_prefab = itemPrefabs[Random.Range(0, itemPrefabs.Length)]; //아이템 목록중에 하나 정함
 
-            Vector3 spawnPos = transform.position + new Vector3(Random.Range(-0.2f, 0.2f), -0.2f, 0);
+            Vector3 spawnPos = transform.position + new Vector3(Random.Range(-0.2f, 0.2f), -0.2f, 0); //아이템 나올 위치 랜덤으로 설정
 
-            GameObject item = Instantiate(item_prefab, spawnPos, Quaternion.identity);
+            GameObject item = Instantiate(item_prefab, spawnPos, Quaternion.identity); //아이템 생성
 
             Rigidbody2D rb = item.GetComponent<Rigidbody2D>();
-            if (rb != null)
+            if (rb != null) //rigidbody 존재시
             {
                 // 위로 + 옆으로 약간 튀는 랜덤 힘
                 Vector2 force = new Vector2(Random.Range(-1f, 1f), 1f) * dropForce;
--- LucidArcher_Project/Assets/02. Scripts/ItemManager.cs
+++ LucidArcher_Project/Assets/02. Scripts/ItemManager.cs
@@ -8,17 +8,17 @@ public abstract class ItemManager : MonoBehaviour
     //private string itemName; //아이템 이름인데 쓸 진 아직 모름 아이템 설명 필요할 때 쓸 수도?
 
 
-    // Update is called once per frame
-    public float pickupDelay = 0.5f;
-    public float spawnTime;
+
+    public float pickupDelay = 0.5f; // 아이템 바로 먹어지면 안되니 딜레이
+    public float spawnTime; // 아이템 생성된 시간 변수
 
 
     private void Start()
     {
-        spawnTime = Time.time; 
+        spawnTime = Time.time;  //아이템 생성시 시간 기록
     }
 
-    public abstract void ItemAction(GameObject player); 
+    public abstract void ItemAction(GameObject player);  //자식 클래스에서 구현 될 메서드
 
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,11 +26,12 @@ public abstract class ItemManager : MonoBehaviour
         if (other.CompareTag("Player"))
         {
 
-            if(Time.time -spawnTime< pickupDelay) {
+            if (Time.time - spawnTime < pickupDelay)  // 스폰되고 부딪힌 시간이 pickupDelay보다 빠르면 안먹어짐
+            { 
                 return;
 
-        }
-            ItemAction(other.gameObject);
+            }
+            ItemAction(other.gameObject); //아이템 고유 효과 발동
 
         }
     }
