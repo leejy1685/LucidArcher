27df681637757e89bd0112d4ad883dcc6d947585
leejy1685 <ucc1685@naver.com>
2025-05-09 17:10:42 +0900

    refactor : 플레이어와 무기에 있던 스텟을 분리
    PlayerStatHendler.cs
    WeaponStat.cs 생성

---
+++ LucidArcher_Project/Assets/02. Scripts/Weapon/WeaponStat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStat : MonoBehaviour
+{
+    [SerializeField] int bulletNum;    //화살 수
+    public int BulletNum { get { return bulletNum; } }
+    [SerializeField] float damage;    //대미지
+    public float Damage { get { return damage; } }
+    [SerializeField] float bulletSpeed; //화살속도
+    public float BulletSpeed { get { return bulletSpeed; } }
+
+    //넉백 시간과 파워
+    [SerializeField] float knockbackDuration = 0.5f;
+    public float KnockbackDuration { get { return knockbackDuration; } }
+    [SerializeField] float knockbackPower = 1f;
+    public float KnockbackPower { get { return knockbackPower; } }
+}
