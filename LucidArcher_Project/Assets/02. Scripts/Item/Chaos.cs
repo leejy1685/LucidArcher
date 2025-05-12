b3da1ff401ee28d147651922c502d590c8e149e6
hbks0224 <hbks1234@gmail.com>
2025-05-12 20:50:02 +0900

    몬스터 아이템 드랍

---
+++ LucidArcher_Project/Assets/02. Scripts/Item/Chaos.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaos : ItemManager
+{
+    public override void ItemAction(GameObject player)
+    {
+
+
+
+    }
+}
--- LucidArcher_Project/Assets/02. Scripts/Monsters/Based/MonsterBase.cs
+++ LucidArcher_Project/Assets/02. Scripts/Monsters/Based/MonsterBase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public abstract class MonsterBase : MonoBehaviour
 {
@@ -22,6 +23,11 @@ public abstract class MonsterBase : MonoBehaviour
 
     //�˹�
     KnockbackApplier knockbackApplier;
+
+    public GameObject[] itemPrefabs; //아이템 목록 담을 리스트
+
+
+    public float dropForce = 2f; //아이템 튀어오를때 힘
 
     protected virtual void Start()
     {
@@ -62,6 +68,7 @@ public abstract class MonsterBase : MonoBehaviour
     void Die()
     {
         monsterSpawner.MonsterCount--;
+        DropItems();
         gameObject.SetActive(false);
     }
 
@@ -94,6 +101,39 @@ public abstract class MonsterBase : MonoBehaviour
     {
         if ((transform.position - detectedEnemy.transform.position).magnitude <= stats.Range) return true;
         return false;
+
+    }
+
+    void DropItems()
+    {
+
+        int itemcount = Random.Range(1, 6); //드랍 아이템 개수
+
+        for (int i = 0; i < itemcount; i++) // 드랍 아이템 수 만큼 반복
+        {
+            GameObject item_prefab = itemPrefabs[Random.Range(0, itemPrefabs.Length)]; //아이템 목록중에 하나 정함
+
+            Vector3 spawnPos = transform.position + new Vector3(Random.Range(-0.15f, 0.15f), -0.2f, 0); //아이템 나올 위치 랜덤으로 설정
+
+            GameObject item = Instantiate(item_prefab, spawnPos, Quaternion.identity); //아이템 생성
+
+            Rigidbody2D rb = item.GetComponent<Rigidbody2D>();
+            if (rb != null) //rigidbody 존재시
+            {
+                // 위로 + 옆으로 약간 튀는 랜덤 힘
+                Vector2 force = new Vector2(Random.Range(-1f, 1f), 1f) * dropForce;
+                rb.AddForce(force, ForceMode2D.Impulse);
+            }
+
+            ItemManager drop = item.GetComponent<ItemManager>();
+            if (drop != null)
+            {
+
+                drop.itemY = transform.position.y;
+            }
+
+        }
+
 
     }
 }
