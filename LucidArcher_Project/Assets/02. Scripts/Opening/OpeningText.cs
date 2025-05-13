99dd584a81eaf2854ba4b194e47ce208d251fee1
hbks0224 <hbks1234@gmail.com>
2025-05-13 21:01:29 +0900

    애니메이션 및 자막 추가

--- LucidArcher_Project/Assets/02. Scripts/Opening/OpeningText.cs
+++ LucidArcher_Project/Assets/02. Scripts/Opening/OpeningText.cs
@@ -1,20 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpeningText : MonoBehaviour
 {
+    public Text subtitleText;        // UI 텍스트
+    public string[] lines;           // 자막 내용 배열
+    public float[] delays;           // 각 줄 대기 시간
+    public float typingSpeed = 0.05f; // 한 글자당 출력 속도
 
-    
-    public float duration = 3.0f;
     void Start()
     {
-        
+        StartCoroutine(ShowSubtitlesWithTyping());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShowSubtitlesWithTyping()
     {
-        
+        for (int i = 0; i < lines.Length; i++)
+        {
+            subtitleText.text = "";
+            yield return StartCoroutine(TypeLine(lines[i]));
+            yield return new WaitForSeconds(delays[i]);
+        }
+
+        subtitleText.text = ""; // 마지막 자막 비우기 (선택)
+    }
+
+    IEnumerator TypeLine(string line)
+    {
+        foreach (char c in line)
+        {
+            subtitleText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 }
