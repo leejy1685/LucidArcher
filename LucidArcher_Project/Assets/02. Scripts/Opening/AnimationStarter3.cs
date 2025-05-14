020e52c9f42b18b7ba93ad966f707522df88b299
hbks0224 <hbks1234@gmail.com>
2025-05-14 17:26:26 +0900

    엔딩씬 추가

---
+++ LucidArcher_Project/Assets/02. Scripts/Opening/AnimationStarter3.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationStarter3 : MonoBehaviour //애니메이션 재생을 위한 스크립트
+{
+
+
+    public AudioSource main; // 메인화면 브금
+    public Animator anim1; // 눈깜빡 애니메이션
+
+
+
+    void Start()
+    {
+        StartCoroutine(PlayAnimations());
+    }
+
+    IEnumerator PlayAnimations()
+    {
+
+        
+        yield return new WaitForSeconds(16f); 
+
+        anim1.SetTrigger("Ending");
+        yield return new WaitForSeconds(15f);
+
+
+        SceneManager.LoadScene("IntroMenuScene");
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("IntroMenuScene");
+
+        }
+
+
+    }
+
+
+}
